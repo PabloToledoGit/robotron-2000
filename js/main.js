25df4af9aec92617c38680bf4ec0c8e0537a2a5c
PabloToledoGit <pablotoledoemail@gmail.com>
2023-07-21 11:52:10 -0300

    estudando

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ const subtrair = document.querySelector('#subtrair');
 const somar = document.querySelector('#somar');
 const braco = document.querySelector('#braco');
 
+const controle = document.querySelectorAll('.controle-ajuste');
+
+controle.forEach( (elemento) =>{
+    elemento.addEventListener('click', (evento) =>{
+        manipulaDados(evento.target.textContent)
+    })
+})
+
 somar.addEventListener('click', () =>{
 
  manipulaDados(somar)
@@ -16,7 +24,7 @@ subtrair.addEventListener('click', () =>{
 
 function manipulaDados(operacao){
 
-    if(operacao === subtrair){
+    if(operacao === "-"){
         braco.value = parseInt(braco.value) - 1
     }else{
         braco.value = parseInt(braco.value) + 1
